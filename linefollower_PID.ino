c4686aa74bf9e4a0951a4d4bb1ae9354a4bf4afc
Diego Garcia Garcia <diegodevigo@gmail.com>
2018-12-01 15:18:47 +0100

    Add files via upload

--- linefollower_PID.ino
+++ linefollower_PID.ino
@@ -1,22 +1,30 @@
+#include <Statistic.h>
 #include <PID_v1.h>
 #include <Wire.h>
 #define uchar unsigned char
 uchar t;
 int calib[8];
 uchar data[16];
-double Kp=0.25, Ki=10, Kd=5 setpoint, va, vb, pos;
-PID motorA(&pos, &va, &setpoint, Kp, Ki, Kd, DIRECT);
-PID motorB(&pos, &vb, &setpoint, Kp, Ki, Kd, DIRECT);
+double setpoint, va, vb, pos;
+double Kpa = 0.25, Kia = 10, Kda = 5, Kpb = 0.25, Kib = 10, Kdb = 5;
+PID motorA(&pos, &va, &setpoint, Kpa, Kia, Kda, DIRECT);
+PID motorB(&pos, &vb, &setpoint, Kpb, Kib, Kdb, DIRECT);
+Statistic highSens[8];
+Statistic lowSens[8];
 
 void setup() {
   Wire.begin();
   Serial.begin(9600);
   calibrateSensors(calib);
   setpoint = 0;
-  pos=0;
+  pos = 0;
   motorA.SetMode(AUTOMATIC);
   motorB.SetMode(AUTOMATIC);
   t = 0;
+  for (int a = 0; a < 8; a++) {
+    highSens[a].clear();
+    lowSens[a].clear();
+  }
 }
 void loop() {
   int sensor[8];
@@ -39,10 +47,10 @@ void loop() {
 
 
 
-
 void getSensorValues(int sensor[]) {
   int x = 0;
   Wire.requestFrom(9, 16);
+  Serial.println();
   while (Wire.available()) {
     data[t] = Wire.read();
     if (t % 2 == 0) {
@@ -68,39 +76,44 @@ void calibrateSensors(int calibrationValues[]) {
   int sensorLow[8];
   int sensorHigh[8];
   getSensorValues(sensor);
-  for (int a = 0; a < 8 ; a++) {
-    sensorHigh[a] = 0;
-    sensorLow[a] = 255;
+  delay(1000);
+  getSensorValues(sensor);
+  for (int a = 0; a < 8; a++) {
+    lowSens[a].add(sensor[a]);
+    highSens[a].add(sensor[a]);
   }
   int rep = 1;
-  while (rep < 50) {
+  do {
     delay(100);
     getSensorValues(sensor);
     for (int a = 0; a < 8; a++) {
-      if (sensor[a] < sensorLow[a] && sensor[a] > 0) {
-        sensorLow[a] = sensor[a];
+      if (sensor[a] < lowSens[a].average() && sensor[a] > 0) {
+        lowSens[a].add(sensor[a]);
       }
-      if (sensor[a] > sensorHigh[a] && sensor[a] < 255) {
-        sensorHigh[a] = sensor[a];
+      if (sensor[a] > highSens[a].average() && sensor[a] < 255) {
+        highSens[a].add(sensor[a]);
       }
     }
     rep++;
-  }
+  } while (rep < 50);
+
   Serial.print("Sensor high: ");
   for (int a = 0; a < 8; a++) {
+    sensorHigh[a] = highSens[a].average();
     Serial.print(sensorHigh[a]);
     Serial.print("  ");
   }
   Serial.println();
   Serial.print("Sensor low: ");
   for (int a = 0; a < 8; a++) {
+    sensorLow[a] = lowSens[a].average();
     Serial.print(sensorLow[a]);
     Serial.print("  ");
   }
   Serial.println();
   Serial.print("Calibration values:  ");
   for (int a = 0; a < 8; a++) {
-    calibrationValues[a] = (sensorHigh[a] - sensorLow[a])/2;
+    calibrationValues[a] = sensorLow[a] + (sensorHigh[a] - sensorLow[a]) / 2;
     Serial.print(calibrationValues[a]);
     Serial.print("  ");
   }
@@ -110,7 +123,7 @@ void calibrateSensors(int calibrationValues[]) {
 void transformBinary(int sensor[], int calib[]) {
   Serial.print("Sensor binary: ");
   for (int a = 0; a < 8; a++) {
-    sensor[a]=(sensor[a] > calib[a]) ? 0 : 1;
+    sensor[a] = (sensor[a] > calib[a]) ? 0 : 1;
     Serial.print(sensor[a]);
     Serial.print("  ");
   }
@@ -126,4 +139,15 @@ int linePos(int sensor[]) {
   }
   return k;
 }
-
+/*
+  Statistic();    // constructor
+  void clear();   // reset all counters
+  void add(double); // add a new value
+  long count();   // # values added
+  double sum();   // total
+  double minimum(); // minimum
+  double maximum(); // maximum
+  double average(); // average
+  double pop_stdev(); // population std deviation
+  double unbiased_stdev();  // unbiased std deviation
+*/
